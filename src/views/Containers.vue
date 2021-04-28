e32e26adb5e8d3978d0cecde81781ed8f9d15012
Salvatore Uras <yogavidya@gmail.com>
2021-04-28 06:42:17 +0200

    Start

---
+++ src/views/Containers.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        Hello World
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.container {
+    background-color: yellow;
+    border: 1px solid black;
+}
+
+</style>
